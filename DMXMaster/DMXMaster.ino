0bcfad1c624c8b60fde28581b71df59e7e6dcecf
patgadget <patgadget@yahoo.ca>
2017-12-29 21:17:35 -0500

    Finally Working DMX Out

--- DMXMaster/DMXMaster.ino
+++ DMXMaster/DMXMaster.ino
@@ -13,41 +13,7 @@
 #define RxLCD 6
 #define TxLCD 5
 #define ledPin 13                // choose the pin for the LED
-#define clockPin 7              //clock for serial to parallel to serial
-#define latchPin 9              // after serial light latch them out
-#define dataPin 8                // for LED
-#define KeyOK 10
-#define KeyESC 10
-#define EncCHA 2                // choose the input pin (for a pushbutton)
-#define EncCHB 3                // choose the input pin (for a pushbutton)
-#define loadPin 4
-#define sInDataPin 6
 SoftwareSerial softserial(RxLCD, TxLCD);
-
-
-
-//Define the LED value
-#define TOP_LEFT_RED 0 //0x8000
-#define TOP_LEFT_GREEN 1 //0x4000
-#define TOP_RIGHT_RED 2 //0x2000
-#define TOP_RIGHT_GREEN 3 //0x1000
-#define BOT_LEFT_RED 4 //0x0800
-#define BOT_LEFT_GREEN 5 //0x0400
-#define BOT_RIGHT_RED 6 //0x0200
-#define BOT_RIGHT_GREEN 7 //0x0100
-#define ESC_RED 8 //0x0080
-#define ESC_GREEN 9 //0x0040
-#define OK_RED 10  //0x0020
-#define OK_GREEN 11  //0x0010
-
-//Define the input value
-#define BOT_RIGHT 0
-#define BOT_LEFT 1
-#define TOP_RIGHT 2
-#define TOP_LEFT 3
-#define OK 6
-#define ESC 7
-
 
 #define myubrr (16000000L/16/250000-1)
 
@@ -95,7 +61,6 @@ int ReadTimeTap;
 int TapSyncTime;
 int LastTapSyncTime;
 
-int pinto=0;
 
 
 
@@ -103,24 +68,9 @@ int pinto=0;
 /*************************************************************/
 void setup()
 {
-  // Config the LED output for the serial to paralel chip
-  pinMode(clockPin, OUTPUT);      
-  pinMode(latchPin, OUTPUT);
-  pinMode(dataPin,OUTPUT);
-  // Config the serial input switch
-  pinMode(loadPin, OUTPUT);
-  pinMode(sInDataPin,INPUT);
-
-  //Config of the rotary encoder
-  pinMode(EncCHA, INPUT);
-  pinMode(EncCHB, INPUT);
   pinMode(1, OUTPUT);
   pinMode(ledPin, OUTPUT);      // declare LED as output
   pinMode(TxLCD, OUTPUT);
-  pinMode(KeyOK, INPUT);
-  pinMode(KeyESC, INPUT);
-  digitalWrite(KeyOK, HIGH); // Turn ON the pull up resistor
-  digitalWrite(KeyESC, HIGH);
 
   digitalWrite(TxLCD, HIGH);
   delay (100);
@@ -132,7 +82,15 @@ void setup()
   softserial.write(17); // turn on back light
   delay (50);
   softserial.write(12); // Clear Screen
-  softserial.print(" DMX Console Master");
+  softserial.print(" DMX Console Master ");
+  UCSR0A &= (~0x02); // Cancelling Doubling Bit
+
+  if ( UCSR0A & 0x02)
+    softserial.print("Doubling ON");
+  else
+    softserial.print("NO Doubling");
+  
+  
   delay (500);
 
   //  To Change speed to 250Kb and 2 stop bit\
@@ -140,12 +98,13 @@ void setup()
   UBRR0H = (unsigned char)(myubrr>>8); //High part, only bit 0-3
   UBRR0L = (unsigned char)myubrr; //Low part
   //UCSR0B |= ((1<<RXEN0)|(1<<RXCIE0));//Enable Receiver and Interrupt RX
+  
+  
   UCSR0C |= ((3<<UCSZ00)|(1<<USBS0)); //N81 No parity/8 bits/2 Stop bit
+
   
   //UBRR0L = 3;
   //UCSR0C = UCSR0C | 0x08;
-
-  randomSeed(analogRead(0));
   }
 
 
@@ -156,26 +115,29 @@ void setup()
 /*************************************************************/
 void loop()
 {
-  int returnValue;
-
   if (full_dmx_channel == 0) //Make a break in serial
   {
-    UCSR0B = UCSR0B & ~0x08;
-    delayMicroseconds(120);
+    UCSR0B = UCSR0B & ~0x08; // Disable USART Transmit
+    //delayMicroseconds(120);
     pinMode(1, OUTPUT);
-    digitalWrite(1,LOW);
-    delayMicroseconds(12000);
+    //digitalWrite(1,HIGH);
+    //delayMicroseconds(120);
+    digitalWrite(1,LOW); // Low = Break
+    delayMicroseconds(120);
     digitalWrite(1,HIGH);
-    delayMicroseconds(12);
-    UCSR0B = UCSR0B | 0x08;
+    UCSR0B = UCSR0B | 0x08; // Enable USART Transmit
     Serial.write(0x00);
    }
-  if (full_dmx_channel>0) //Not a break then write the DMX Channel
+  //if (full_dmx_channel>0) //Not a break then write the DMX Channel
+  if (full_dmx_channel == 1) //Not a break then write the DMX Channel
     {
+    Serial.write(0xF0);
+    Serial.write(0x0F);
+    Serial.write(0x00);
     Serial.write(0xFF);
     }
   full_dmx_channel++;
-  if (full_dmx_channel>50)
+  if (full_dmx_channel>5)
     {
     full_dmx_channel = 0;
     readPot1=analogRead(0);
